695209d662b635e9786428fa7ee7e305403cd18c
murasaki <yankvasya@gmail.com>
2022-10-21 18:26:36 +0400

    feat: [components] create CustomList

---
+++ components/CustomList.vue
@@ -0,0 +1,52 @@
+<template>
+  <li class="list">
+    <h2 class="list__title">{{ list.name }}</h2>
+
+    <ul class="list__tasks tasks">
+      <CustomItem
+        v-for="task of tasks"
+        :key="task.id"
+        :item="task"
+        class="tasks__task"
+      />
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'CustomList',
+  props: {
+    list: {
+      default: () => ({ id: 1, name: 'List' }),
+      type: Object,
+    },
+    tasks: {
+      default: () => [],
+      type: Array,
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.list {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  border: 1px solid black;
+  padding: 10px 50px;
+
+  &__title {
+    text-align: center;
+  }
+
+  &__tasks {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    border: 1px solid black;
+    padding: 10px 10px 80px;
+  }
+}
+</style>
