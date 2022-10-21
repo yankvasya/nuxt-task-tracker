107d900c6b6aa537e986b99d8ba0c93ad5781c99
murasaki <yankvasya@gmail.com>
2022-10-21 18:26:26 +0400

    feat: [components] create CustomItem

---
+++ components/CustomItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="task">
+    <h4 class="task__title">{{ item.name }}</h4>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'CustomList',
+  props: {
+    item: {
+      type: Object,
+      default: () => ({
+        id: 1,
+        name: 'Task',
+      }),
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.task {
+  padding: 8px;
+  border: 1px solid black;
+  border-radius: 5px;
+}
+</style>
