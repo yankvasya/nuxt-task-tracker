9f84053fccb22051bfc52c22cf5ace2470f1d294
murasaki <yankvasya@gmail.com>
2022-10-21 19:06:40 +0400

    feat: [components/CustomItem] add startDrag emit

--- components/CustomItem.vue
+++ components/CustomItem.vue
@@ -1,5 +1,11 @@
 <template>
-  <li class="task">
+  <li
+    class="task"
+    :class="{ 'task--drag': isDrag }"
+    draggable="true"
+    @dragstart="startDrag($event)"
+    @dragend="endDrag()"
+  >
     <h4 class="task__title">{{ item.name }}</h4>
   </li>
 </template>
@@ -16,13 +22,34 @@ export default {
       }),
     },
   },
+  emits: ['update:drag'],
+  data() {
+    return {
+      isDrag: false,
+    }
+  },
+  methods: {
+    startDrag(event) {
+      this.isDrag = true
+      this.$emit('update:drag', event, this.item)
+    },
+    endDrag() {
+      this.isDrag = false
+    },
+  },
 }
 </script>
 
 <style lang="scss">
 .task {
+  cursor: pointer;
   padding: 8px;
   border: 1px solid black;
   border-radius: 5px;
+
+  &--drag {
+    transform: scale(1.1);
+    background: rgba(128, 128, 128, 0.3);
+  }
 }
 </style>
