83871f3e26b1b34e5efeec10a56c8cff2d793abf
murasaki <yankvasya@gmail.com>
2022-10-21 18:27:11 +0400

    feat: [index.vue] create static app

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,77 @@
 <template>
-  <div class="wrapper"></div>
+  <div class="wrapper">
+    <h1 class="wrapper__title">Nuxt task tracker</h1>
+
+    <ul class="wrapper__list">
+      <CustomList
+        v-for="list of lists"
+        :key="list.key"
+        :list="list"
+        :tasks="currentTasks(list)"
+      />
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      lists: [
+        { id: 1, name: 'Task' },
+        { id: 2, name: 'In progress' },
+        { id: 3, name: 'Done!' },
+      ],
+      tasks: [
+        {
+          id: 1,
+          name: 'Task 1',
+          status: 1,
+        },
+        {
+          id: 2,
+          name: 'Task 2',
+          status: 1,
+        },
+        {
+          id: 3,
+          name: 'Task 3',
+          status: 2,
+        },
+        {
+          id: 4,
+          name: 'Task 4',
+          status: 3,
+        },
+      ],
+    }
+  },
+  computed: {
+    currentTasks: (data) => (list) => {
+      return data.tasks.filter((task) => task.status === list.id)
+    },
+  },
 }
 </script>
 
-<style scoped lang="scss" />
+<style scoped lang="scss">
+.wrapper {
+  height: 100vh;
+  border: 1px solid black;
+
+  &__title {
+    text-align: center;
+  }
+
+  &__list {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    gap: 10px;
+    border: 1px solid black;
+    padding: 10px;
+    height: 90%;
+  }
+}
+</style>
